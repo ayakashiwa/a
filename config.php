e0be26e020781a2d522c8f6493ba7de0c76d963e
ayakashiwa <ayksw122@gmail.com>
2022-03-06 16:49:03 +0900

    Add README.md

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-define('DSN', 'mysql:host=db;dbname=a_db;charset=utf8;');
-define('USER', 'a_admin');
+define('DSN', 'mysql:host=db;dbname=admire_db;charset=utf8;');
+define('USER', 'admire_admin');
 define('PASSWORD', '6112');
